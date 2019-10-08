a6529987c44a2c00f6ffe4f20957a46141678a9d
ASR27 <albertoasr27@hotmail.com>
2019-10-08 19:49:30 +0200

    modelos retocados

--- hojapersonaje/admin.py
+++ hojapersonaje/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 from .models import Caracteristica
+from .models import Usuario
+from .models import Campaña
+from .models import Participa
 
 admin.site.register(Caracteristica)
+admin.site.register(Usuario)
+admin.site.register(Campaña)
+admin.site.register(Participa)
 
 # Register your models here.
--- hojapersonaje/models.py
+++ hojapersonaje/models.py
@@ -7,10 +7,16 @@ class Usuario(models.Model):
 	nomUsu = models.CharField(max_length=40)
 	email = models.EmailField(max_length=200)
 
+	def __str__(self):
+		return self.nomUsu
+
 class Campaña(models.Model):
 	idCam = models.AutoField(primary_key=True)
 	nomCam = models.CharField(max_length=100)
 	desCam = models.CharField(max_length=5000)
+
+	def __str__(self):
+		return self.nomCam
 
 class Personaje(models.Model):
 	idPer  = models.AutoField(primary_key=True)
@@ -21,36 +27,61 @@ class Personaje(models.Model):
 	genPer = models.CharField(max_length=10)
 	aliPer = models.CharField(max_length=20)
 
+	def __str__(self):
+		return self.nomPer
+
 class Caracteristica(models.Model):
 	idCar = models.AutoField(primary_key=True)
 	nomCar = models.CharField(max_length=20)
 	abCar = models.CharField(max_length=3)
+
+	def __str__(self):
+		return self.nomCar
 
 class Habilidad(models.Model):
 	idHab = models.AutoField(primary_key=True)
 	nomHab = models.CharField(max_length=30)
 	modHab = models.CharField(max_length=3)
 
+	def __str__(self):
+		return self.nomHab
+
 class Dote(models.Model):
 	idDot = models.AutoField(primary_key=True)
 	nomDot = models.CharField(max_length=100)
 	desDot = models.CharField(max_length=5000)
 
+	def __str__(self):
+		return self.nomDot
+
 class Participa(models.Model):
-	usuPar = models.ManyToManyField(Usuario)
-	camPar = models.ManyToManyField(Campaña)
+	usuPar = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+	camPar = models.ForeignKey(Campaña, on_delete=models.CASCADE)
 	dmPar = models.BooleanField(default=False)
+
+	def __str__(self):
+		return str(self.camPar)
 
 class Rasgo(models.Model):
 	perRas = models.ManyToManyField(Personaje)
 	carRas = models.ManyToManyField(Caracteristica)
 	valRas = models.IntegerField(2)
 
+	def __str__(self):
+		return self.perRas
+
 class Rango(models.Model):
 	perRan = models.ManyToManyField(Personaje)
 	habRan = models.ManyToManyField(Habilidad)
 	valRan = models.IntegerField(2)
 
+	def __str__(self):
+		return self.perRan
+
 class Cualidad(models.Model):
 	perCua = models.ManyToManyField(Personaje)
 	dotCua = models.ManyToManyField(Dote)
+
+	def __str__(self):
+		return self.perCua
+
